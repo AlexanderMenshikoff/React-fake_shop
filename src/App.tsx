623cb6c7e2144910b302835c0e72fb483be05707
Alexander <alexmenshikov7@gmail.com>
2024-04-04 22:38:23 +0500

    added component ProductsList

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { ProductsList } from "./components/ProductsList";
 
 function App() {
-  return <div className="App">Yo</div>;
+  return (
+    <div className="App">
+      <ProductsList />
+    </div>
+  );
 }
 
 export default App;
