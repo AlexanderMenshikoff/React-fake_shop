f5146ef5240dad60b74a0b369d87af03eb6d2f76
Alexander <alexmenshikov7@gmail.com>
2024-04-05 16:45:08 +0500

    made "show more" button functional

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -4,33 +4,63 @@ import { useEffect, useState } from "react";
 
 export function ProductsList() {
   const [products, setProducts] = useState<IProductsList[]>([]);
+  const [showProducts, setShowProducts] = useState(12);
 
   async function getData() {
     const response = await axios.get<IProductsList[]>(
-      "https://api.escuelajs.co/api/v1/products?offset=0&limit=21"
+      `https://api.escuelajs.co/api/v1/products?offset=0&limit=${showProducts}`
     );
     setProducts(response.data);
   }
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [showProducts]);
+
+  console.log(products);
+  console.log(showProducts);
 
   return (
     <>
       <div className="flex flex-wrap items-center gap-[60px] max-w-[1200px] ml-auto mr-auto">
-        {products.map((el) => (
-          <div className="w-[30%] text-center " key={el.id}>
-            <img
-              className="w-4/4 ml-auto mr-auto"
-              src={el.images[0]}
-              alt={el.title}
-            />
-            <div className=" ml-auto mr-auto">{el.title}</div>
-            <div className=" ml-auto mr-auto">{el.price}$</div>
-          </div>
-        ))}
+        {products.map((el) =>
+          el.id <= 51 ? (
+            <div className="w-[30%] text-center " key={el.id}>
+              <img
+                className="w-4/4 ml-auto mr-auto"
+                src={el.images[0]}
+                alt={el.title}
+              />
+              <div className=" ml-auto mr-auto">{el.title}</div>
+              <div className=" ml-auto mr-auto">{el.price}$</div>
+            </div>
+          ) : (
+            <div className="w-[30%] text-center " key={el.id}>
+              <img
+                className=" ml-auto mr-auto"
+                src={el.category.image}
+                alt={el.title}
+              />
+              <div className=" ml-auto mr-auto ">{el.title}</div>
+              <div className=" ml-auto mr-auto">{el.price}$</div>
+            </div>
+          )
+        )}
       </div>
+      {showProducts <= products.length ? (
+        <div className="flex justify-center">
+          <button
+            className="ml-auto mr-auto mt-[40px] mb-[40px] border-2 rounded-[25px] py-[10px] px-[20px]"
+            onClick={() => {
+              setShowProducts((prev) => prev + 12);
+            }}
+          >
+            Show more
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
