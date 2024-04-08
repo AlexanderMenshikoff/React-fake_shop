e2bfd16b6a111e9970c83b2eb83cd43802cc726e
Alexander <alexmenshikov7@gmail.com>
2024-04-08 19:20:06 +0500

    added new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 import { ProductsList } from "./components/ProductsList";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <ProductsList />
+      <Footer />
     </div>
   );
 }
