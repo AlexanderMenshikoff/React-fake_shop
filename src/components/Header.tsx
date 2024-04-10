382aacb1e568db3985c3692f67479c9774ba21ad
Alexander <alexmenshikov7@gmail.com>
2024-04-10 14:31:38 +0500

    added links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom";
 import { FakeLogo } from "./FakeLogo";
 
 export function Header() {
   return (
     <div className="bg-black opacity-90 h-[80px] fixed w-[100%] flex items-center justify-center">
       <nav className="flex justify-center items-center ">
-        <FakeLogo template="w-[220px] h-[130px] fixed left-[0px]" />
+        <Link to="/">
+          <FakeLogo template="w-[220px] h-[130px] fixed left-[0px] top-[0px]" />
+        </Link>
+
         <ul className="text-white flex gap-[10px]">
           <li>
-            <a href="/">About us</a>
+            <Link to="/about">About us</Link>
           </li>
           <li>
-            <a href="/">Delivery</a>
+            <Link to="/delivery">Delivery</Link>
           </li>
         </ul>
       </nav>
