d0a5ebb71c561fea68aa10ecf8faba39d280d7d7
Alexander <alexmenshikov7@gmail.com>
2024-04-10 14:30:39 +0500

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-import { ProductsList } from "./components/ProductsList";
+import { AboutPage } from "./pages/AboutPage";
+import { DeliveryPage } from "./pages/DeliveryPage";
+import { MainPage } from "./pages/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <ProductsList />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/delivery" element={<DeliveryPage />} />
+      </Routes>
     </div>
   );
 }
