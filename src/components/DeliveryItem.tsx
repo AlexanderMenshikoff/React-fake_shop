41f4c42c40be3a138ab895135c27a120d94eeea2
Alexander <alexmenshikov7@gmail.com>
2024-04-11 16:22:05 +0500

    created brand new delivery item component

---
+++ src/components/DeliveryItem.tsx
@@ -0,0 +1,15 @@
+export interface IDeliveryItem {
+  img: string;
+  mainText: string;
+  subText: string;
+}
+
+export function DeliveryItem(props: IDeliveryItem) {
+  return (
+    <div className="w-[30%] text-center border-2 border-stone-950 border-solid min-h-[570px]">
+      <img className="mr-auto ml-auto py-[35px]" src={props.img} alt="" />
+      <p className="pb-[30px]">{props.mainText}</p>
+      <p className="pb-[50px] px-[35px]">{props.subText}</p>
+    </div>
+  );
+}
