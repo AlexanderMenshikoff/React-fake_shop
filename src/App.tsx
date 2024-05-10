26cca8c272ecf15a8f31da64ca875ebe264fa1b2
Alexander <alexmenshikov7@gmail.com>
2024-05-10 16:35:05 +0500

    added routh path for product page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { AboutPage } from "./pages/AboutPage";
 import { DeliveryPage } from "./pages/DeliveryPage";
 import { MainPage } from "./pages/MainPage";
 import { ScrollToTop } from "./functions/functions";
+import { ProductPage } from "./pages/ProductPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/delivery" element={<DeliveryPage />} />
+        <Route path="/product" element={<ProductPage />} />
       </Routes>
     </div>
   );
