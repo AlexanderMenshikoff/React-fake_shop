c4b8f2c6091d5dbe1e160bffd08a43fce391544c
Alexander <alexmenshikov7@gmail.com>
2024-05-12 14:33:11 +0500

    a minor upgrade for getData func, added inputValue state & brand new SearchingInput component, also configured component to search products & sm btn appereance

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,19 +1,23 @@
 import axios from "axios";
 import { IProductsList } from "../interfaces/interfaces";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { ProductItem } from "./ProductItem";
+import { SearchingInput } from "./SearchingInput";
 
 export function ProductsList() {
   const [products, setProducts] = useState<IProductsList[]>([]);
   const [showProducts, setShowProducts] = useState(12);
+  const [inputValue, setInputValue] = useState("");
 
   async function getData() {
     const response = await axios.get<IProductsList[]>(
-      `https://fakestoreapi.com/products?offset=0&limit=${showProducts}`
+      "https://fakestoreapi.com/products"
     );
+
     setProducts(response.data);
   }
+
+  const paginationProducts = products.slice(0, showProducts);
 
   useEffect(() => {
     getData();
@@ -21,21 +25,35 @@ export function ProductsList() {
 
   console.log(products);
   console.log(showProducts);
+  console.log(paginationProducts);
 
   return (
     <>
-      <div className="flex flex-wrap items-center gap-[60px] max-w-[1200px] ml-auto mr-auto pt-[350px] ">
-        {products.map((el) => (
-          // <Link to="/about">
-          <ProductItem product={el} />
-          // </Link>
-        ))}
+      <SearchingInput inputValue={inputValue} setInputValue={setInputValue} />
+      <div className="flex flex-wrap items-center gap-[60px] max-w-[1200px] ml-auto mr-auto pt-[100px]">
+        {inputValue
+          ? products.map((el) =>
+              el.title.toLowerCase().includes(inputValue.toLowerCase()) ? (
+                <ProductItem product={el} />
+              ) : (
+                ""
+              )
+            )
+          : paginationProducts.map((el) =>
+              el.title.toLowerCase().includes(inputValue.toLowerCase()) ? (
+                <ProductItem product={el} />
+              ) : (
+                ""
+              )
+            )}
       </div>
 
-      {showProducts <= products.length ? (
+      {inputValue ? (
+        ""
+      ) : showProducts <= products.length ? (
         <div className="flex justify-center">
           <button
-            className="ml-auto mr-auto mt-[60px] mb-[40px] border-2 py-[10px] px-[20px] text-white bg-black"
+            className="ml-auto mr-auto mt-[60px] mb-[40px] border-2 py-[10px] px-[20px] rounded-[10px] text-white bg-black"
             onClick={() => {
               setShowProducts((prev) => prev + 12);
             }}
